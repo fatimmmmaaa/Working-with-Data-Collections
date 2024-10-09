1125e4884f03b1f8bb42124162edb4de5461c9e1
Fatima Rettea <fatimarettea@Fatimas-MBP.lan>
2024-10-09 02:52:53 -0700

    Part 1

--- styles.js
+++ styles.js
@@ -5,26 +5,51 @@
 const csvString =
   "ID,Name,Occupation,Age,IsCool\n42,Bruce,Knight,41,true\n57,Bob,Fry Cook,19,true\n63,Blaine,Quiz Master,58,false\n98,Bill,Doctor's Assistant,26,true";
 //create an array
+
 //part 1
 let cells = [];
-let currentCell = 0;
+let currentCell = 0; 
+let currentRow = 0;
+let table = [];
 
-  // Part 1 code
-  if (cells[currentCell]) {
-    cells[currentCell] += csvString[i];
-  } else {
-    cells[currentCell] = csvString[i];
+function reset () {
+  cells = [];
+  currentCell = 0;
+}
+
+for (let i = 0; i < csvString.length; i++) {
+  //check for new cell
+  if (csvString [i] === ',') {
+    if(currentRow === 0) {
+      columns++;
+    }
+    currentCell = i + 1;
+    continue;
   }
 
+  // checking for a new row
+  if (csvString[i] === "\n") {
+    //reset the array
+    cells = []
+    currentCell = 0;
+    continue;
+    }
+}
 
-    // Part 1
-  // if (
-  //   (currentCell === columns && csvString[i + 1] === '\n') ||
-  //   i + 1 === csvString.length
-  // ) {
-  //   // console.log(cell1, cell2, cell3, cell4);
-  //   console.log(cells.toString());
-  // }
-//}
+  // Part 1 code
+//   if (cells[currentCell]) {
+//     cells[currentCell] += csvString[i];
+//   } else {
+//     cells[currentCell] = csvString[i];
+//   }
 
-console.log(table);
+//   if (
+//     (currentCell === 3  && csvString[i + 1] === "\n") ||
+//     i + 1 === csvString.length
+//   ) {
+//     // console.log(cell1, cell2, cell3, cell4);
+//     console.log(cells.toString());
+// }
+
+// console.log(table);
+
